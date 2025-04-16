b1ab1d1609d68969b36633476469cae36f152aa0
pedro2012sjc <pedro2012sjc@gmail.com>
2025-04-16 13:24:02 +0000

    home com as categorias

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const Color backgroudColor = Color(0xff202123);
   static const Color mainColor = Color(0xFFffa559);
+  static const Color lightBackgroudcolor = Color(0xFF343541);
 }
--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:myapp/data/categories_data.dart';
+import 'package:myapp/ui/home/widgets/categorie_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   // Suggested code may be subject to a license. Learn more: ~LicenseLog:1227724413.
@@ -13,19 +16,37 @@ class HomeScreen extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          spacing: 32,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(child: Image.asset('assets/assets/banners/logo.png', width: 147,)),
-            Text("Boas-vindas !"),
-            TextFormField(),
-            Text("Escolha por categoria"),
-            Image.asset('assets/assets/banners/banner_promo.png'),
-            Text("Bem avaliados")
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            spacing: 32,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset('assets/assets/banners/logo.png', width: 147),
+              ),
+              Text("Boas-vindas !"),
+              TextFormField(),
+              Text("Escolha por categoria"),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 8,
+                  children: List.generate(CategoriesData.listCategories.length, (
+                    index,
+                  ) {
+                    return CategoryWidget(
+                      category: CategoriesData.listCategories[index],
+                    );
+                  }),
+                ),
+              ),
+              Image.asset('assets/assets/banners/banner_promo.png'),
+              Text("Bem avaliados"),
+              SizedBox(height: 64,)
+            ],
+          ),
         ),
       ),
     );
