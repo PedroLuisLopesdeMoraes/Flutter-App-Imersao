7ce1b47ca0b46c15110ec5fb4e2431e2baeb87ed
pedro2012sjc <pedro2012sjc@gmail.com>
2025-04-16 12:42:09 +0000

    Lista de dados das categorias

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -13,6 +13,21 @@ class HomeScreen extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
         ],
       ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          spacing: 32,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(child: Image.asset('assets/assets/banners/logo.png', width: 147,)),
+            Text("Boas-vindas !"),
+            TextFormField(),
+            Text("Escolha por categoria"),
+            Image.asset('assets/assets/banners/banner_promo.png'),
+            Text("Bem avaliados")
+          ],
+        ),
+      ),
     );
   }
 }
