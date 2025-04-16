c7da5d5184f9528c24e144ad3b7c63783ea5030f
pedro2012sjc <pedro2012sjc@gmail.com>
2025-04-16 11:37:23 +0000

    commit inicial

---
+++ lib/ui/splash/splash_screen.dart
@@ -0,0 +1,42 @@
+// ignore: unused_import
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:myapp/ui/_core/app_colors.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroudColor,
+      body: Stack(
+        children: [
+          Image.asset('assets/assets/banners/banner_splash.png'),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 32,
+                children: [ 
+                  Image.asset('assets/assets/banners/logo.png', width: 192),
+                  Column(
+                    children: [
+                      Text("Um parceiro inovador para sua", style: TextStyle(color: Colors.white, fontSize: 22)),
+                      Text("melhor experiência culinária !", style: TextStyle(color: AppColors.mainColor, fontSize: 22, fontWeight: FontWeight.w500)),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(onPressed: (){}, child: Text("Bora !"))),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
